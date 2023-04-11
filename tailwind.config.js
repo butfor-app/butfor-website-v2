74a6a40242cae6104ec16607153e2fb1339b5bea
Danish <danish.danish.danish2016@gmail.com>
2023-04-11 17:48:51 +0500

    Added poppins fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#124559",
+        primaryLight: "#70A9BF",
+        primaryDark: "#01161E",
+      },
+      spacing: {
+        18: "75px",
+      },
+      fontFamily: {
+        sans: ["Poppins"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
