fb6031192b550ee16cfd112082dca7495bc78d51
danish2016 <danish.danish.danish2016@gmail.com>
2023-04-14 01:56:32 +0500

    fixed client carousel

--- src/components/CompaniesSlider.vue
+++ src/components/CompaniesSlider.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="slider_wrapper">
-    <!-- <div id="slider"> -->
-    <!-- </div> -->
     <Carousel :items-to-show="4" :autoplay="2000" :wrap-around="true">
       <slide v-for="(logo, index) in logos" :key="index">
         <img :src="logo" alt="" />
--- src/components/SolutionsSlider.vue
+++ src/components/SolutionsSlider.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="slider_wrapper">
+    <!-- <SolutionsCard />
     <SolutionsCard />
     <SolutionsCard />
-    <SolutionsCard />
-    <SolutionsCard />
+    <SolutionsCard /> -->
+    <Carousel
+      :items-to-show="4"
+      :autoplay="2000"
+      :wrap-around="true"
+      class="w-[2000px] h-[280px]"
+    >
+      <slide v-for="logo in 10" :key="logo">
+        <!-- {{ logo }} -->
+        <SolutionsCard />
+      </slide>
+    </Carousel>
   </div>
 </template>
 <script setup>
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 import SolutionsCard from "@/components/SolutionsCard.vue";
 </script>
+
 <style lang="postcss" scoped>
 #slider_wrapper {
   @apply flex gap-x-4;
