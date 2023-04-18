63920d61fb7bd6578fb93d942871062e1b7b505b
danish2016 <danish.danish.danish2016@gmail.com>
2023-04-19 00:51:50 +0500

    Moved footer to own component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -79,57 +79,6 @@
         <button>View All</button>
       </div>
     </div>
-    <div id="footer">
-      <div id="top">
-        <div id="left">
-          <img id="logo" src="@/assets/BFlogo.png" alt="" />
-          <div id="desc">
-            Tueri is a software tool designed to automate the process of filing
-            business interruption insurance claims. It uses advanced algorithms
-            and machine learning technology to calculate your losses and provide
-            comprehensive reports in minutes.
-          </div>
-          <div id="social_media">
-            <a href="#"><img src="@/assets/icons/facebook.png" alt="" /></a>
-            <a href="#"><img src="@/assets/icons/twiter.png" alt="" /></a>
-            <a href="#"><img src="@/assets/icons/instagram.png" alt="" /></a>
-          </div>
-        </div>
-        <div id="right">
-          <div class="link_block">
-            <a href="#">Help</a>
-            <a href="#">Claims 101</a>
-            <a href="#">Blogs</a>
-            <a href="#">Case Studies</a>
-            <a href="#">Claims Resource Hub</a>
-          </div>
-          <div class="link_block">
-            <a href="#">Jobs</a>
-            <a href="#">Webinars</a>
-            <a href="#">Security/Compliance</a>
-            <a href="#">Partnerships</a>
-            <a href="#">Contact Us</a>
-          </div>
-          <div class="link_block">
-            <a href="#">Pricing</a>
-            <a href="#">Butfor vs Competitor 1</a>
-            <a href="#">Butfor vs Competitor 2</a>
-            <a href="#">Butfor vs Competitor 3</a>
-          </div>
-          <div class="link_block">
-            <a href="#">View Demo</a>
-            <a href="#">Schedule Demo</a>
-          </div>
-        </div>
-      </div>
-      <div id="bottom">
-        <div>©2023 Tueri. All rights reserved</div>
-        <div id="privacy_terms">
-          <a href="#">Privacy & Policy</a>
-          <a href="#">Terms & Condition</a>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -187,57 +136,6 @@ const features = [
 
 <style lang="postcss" scoped>
 #app_wrapper {
-  @apply w-[1440px] mx-auto;
-  /* > #navbar {
-    > #navbar_top {
-      @apply px-18 h-18 bg-primaryDark;
-      @apply flex items-center justify-between;
-      > :nth-child(1) {
-        @apply flex items-center;
-        > button {
-          @apply h-8 bg-white text-black px-4 rounded-2xl;
-          @apply cursor-pointer;
-        }
-        > #msg {
-          @apply text-white;
-          @apply px-4;
-          @apply cursor-pointer;
-        }
-      }
-      > :nth-child(2) {
-        @apply flex items-center gap-x-12;
-        > button {
-          @apply text-white;
-          @apply px-4;
-          @apply cursor-pointer;
-        }
-      }
-    }
-    > #navbar_bottom {
-      @apply h-24 px-18;
-      @apply flex items-center justify-between;
-      > #left {
-        @apply flex items-center justify-start gap-x-12;
-        > #logo {
-          @apply h-14;
-        }
-        > #popover_group_wrapper {
-          @apply flex items-center gap-x-12;
-          > div {
-            @apply text-primary font-medium;
-            @apply cursor-pointer flex items-center gap-x-2;
-            > img {
-              @apply h-2 w-4;
-            }
-          }
-        }
-      }
-      > button {
-        @apply h-10 bg-primary text-white text-sm font-medium px-4 rounded-md;
-        @apply cursor-pointer;
-      }
-    }
-  } */
   > #heading {
     @apply relative w-full h-[690px] pt-56 px-[360px];
     > #bg {
@@ -342,46 +240,6 @@ const features = [
       > button {
         @apply text-primary text-base font-medium h-11 w-[168px] rounded-full border border-primary;
         @apply cursor-pointer;
-      }
-    }
-  }
-  > #footer {
-    @apply h-[480px] bg-black px-[60px] py-20;
-    @apply text-white flex flex-col items-center justify-between;
-    @apply w-full;
-    > #top {
-      @apply flex items-center gap-x-16 w-full pb-11;
-      @apply border-b-2 border-[#5F5F5F];
-      > #left {
-        @apply w-[460px] flex flex-col items-start justify-start gap-y-6;
-
-        > #logo {
-          @apply h-14;
-        }
-        > #desc {
-          @apply font-medium font-aneklatin;
-        }
-        > #social_media {
-          @apply flex items-center gap-x-5;
-          > img {
-            @apply h-6;
-          }
-        }
-      }
-      > #right {
-        @apply h-full flex items-center gap-x-16;
-        > .link_block {
-          @apply h-full flex flex-col items-start justify-start gap-y-6;
-          > a {
-            @apply text-white  font-aneklatin;
-          }
-        }
-      }
-    }
-    > #bottom {
-      @apply w-full py-9 flex justify-between items-center;
-      > #privacy_terms {
-        @apply flex justify-center items-center gap-x-8;
       }
     }
   }
