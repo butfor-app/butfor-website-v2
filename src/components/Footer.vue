db504b41f0a955c30265573f8469233a4bcc5f64
danish2016 <danish.danish.danish2016@gmail.com>
2023-04-22 23:25:43 +0500

    fixed footer for mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="footer">
     <div id="top">
-      <div id="left">
+      <div id="about">
         <img id="logo" src="@/assets/BFlogo.png" alt="" />
         <div id="desc">
           Tueri is a software tool designed to automate the process of filing
@@ -15,9 +15,9 @@
           <a href="#"><img src="@/assets/icons/instagram.png" alt="" /></a>
         </div>
       </div>
-      <div id="right">
+      <div id="links">
         <div class="link_block">
-          <a href="#">Help</a>
+          <div id="heading">Help</div>
           <a href="#">Claims 101</a>
           <a href="#">Blogs</a>
           <a href="#">Case Studies</a>
@@ -54,44 +54,68 @@
 <script setup></script>
 <style lang="postcss" scoped>
 #footer {
-  @apply hidden;
+  /* @apply hidden; */
+  @apply bg-black px-4 py-10;
+  @apply text-white flex flex-col items-center justify-between;
+  @apply w-full;
   @screen lg {
-    @apply h-[480px] bg-black px-[60px] py-20;
-    @apply text-white flex flex-col items-center justify-between;
-    @apply w-full;
-    > #top {
-      @apply flex items-center gap-x-16 w-full pb-11;
-      @apply border-b-2 border-[#5F5F5F];
-      > #left {
-        @apply w-[460px] flex flex-col items-start justify-start gap-y-6;
-
-        > #logo {
-          @apply h-14;
-        }
-        > #desc {
-          @apply font-medium font-aneklatin;
-        }
-        > #social_media {
-          @apply flex items-center gap-x-5;
-          > img {
-            @apply h-6;
-          }
+    @apply min-h-[480px] px-[60px] py-20;
+  }
+  > #top {
+    @apply flex flex-col items-start gap-y-10 w-full pb-11;
+    @apply border-b-2 border-[#5F5F5F];
+    @screen lg {
+      @apply flex-row items-center gap-x-16;
+    }
+    > #about {
+      @apply flex flex-col items-start justify-start gap-y-6;
+      @screen lg {
+        @apply w-[460px];
+      }
+      > #logo {
+        @apply h-14;
+      }
+      > #desc {
+        @apply font-medium font-aneklatin;
+      }
+      > #social_media {
+        @apply flex items-center gap-x-5;
+        > img {
+          @apply h-6;
         }
       }
-      > #right {
-        @apply h-full flex items-center gap-x-16;
-        > .link_block {
-          @apply h-full flex flex-col items-start justify-start gap-y-6;
-          > a {
-            @apply text-white  font-aneklatin;
+    }
+    > #links {
+      @apply h-full w-full flex items-start justify-around flex-wrap gap-y-4;
+      @screen lg {
+        @apply items-center gap-x-16 flex-nowrap;
+      }
+      > .link_block {
+        @apply h-full flex flex-col items-start justify-start gap-y-2;
+        @apply text-white text-sm font-aneklatin;
+        @screen lg {
+          @apply gap-y-6 text-base;
+        }
+        > :nth-child(1) {
+          @apply font-bold;
+        }
+        > a {
+          @screen lg {
+            @apply text-base;
           }
         }
       }
     }
-    > #bottom {
-      @apply w-full py-9 flex justify-between items-center;
-      > #privacy_terms {
-        @apply flex justify-center items-center gap-x-8;
+  }
+  > #bottom {
+    @apply w-full py-9 flex flex-col justify-between items-center;
+    @screen lg {
+      @apply flex-row;
+    }
+    > #privacy_terms {
+      @apply mt-4 w-full flex justify-between  items-center;
+      @screen lg {
+        @apply m-0 gap-x-8;
       }
     }
   }
