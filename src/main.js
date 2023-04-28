6739b9a19f46995b036f66cb185fe8aae8aa4906
Danish <danish.danish.danish2016@gmail.com>
2023-04-29 00:48:53 +0500

    Added google analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
-import { createHead } from "@vueuse/head"
+import { createPinia } from "pinia";
+import { createHead } from "@vueuse/head";
+import VueGtag from "vue-gtag";
 
 import App from "./App.vue";
 import router from "./router";
@@ -7,12 +9,24 @@ import router from "./router";
 // import './index.css'
 import "./assets/css/tailwind.css";
 
-const app = createApp(App);
-const head = createHead()
+const pinia = createPinia();
+const head = createHead();
 
+const app = createApp(App);
 
 app.use(router);
+app.use(pinia);
 app.use(head);
-
+app.use(
+  VueGtag,
+  {
+    appName: "BUTFOR",
+    pageTrackerScreenviewEnabled: true,
+    config: {
+      id: "G-WW9RL8K8L7",
+    },
+  },
+  router
+);
 
 app.mount("#app");
