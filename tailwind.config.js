492567e6964a64d6ba60282174e4b7f84212c7d8
danish2016 <danish.danish.danish2016@gmail.com>
2023-05-03 01:34:38 +0500

    Fixed fadein anitmation

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,9 @@ module.exports = {
           },
         },
         fadeIn: {
-          "0%": {},
+          "0%": {
+            opacity: 0,
+          },
           "100%": {
             opacity: 1,
           },
