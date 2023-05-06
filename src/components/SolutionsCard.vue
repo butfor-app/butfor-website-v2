21a921193985a35056d49abc86aebe51f8eca657
Danish <danish.danish.danish2016@gmail.com>
2023-05-06 21:54:43 +0500

    updated solution card sizing

--- src/components/SolutionsCard.vue
+++ src/components/SolutionsCard.vue
@@ -31,24 +31,24 @@ const image = new URL(
 </script>
 <style lang="postcss" scoped>
 #solution_card {
-  @apply h-[300px] w-[314px] bg-primary px-7 pt-10 pb-7 cursor-grab;
-  @apply flex flex-col items-center justify-center gap-y-4;
+  @apply h-[300px] w-[334px] bg-primary px-4 pt-8 pb-7 cursor-grab;
+  @apply flex flex-col items-center justify-start;
   @apply rounded-2xl;
   @apply shadow-lg text-white font-aneklatin;
   @screen lg {
-    @apply h-[276px] w-[354px];
+    @apply h-[276px] w-[354px] px-6 pt-10;
   }
   > #icon {
-    @apply w-12 h-12;
+    @apply w-12 h-12 mb-4;
     > img {
       @apply w-full h-full;
     }
   }
   > #heading {
-    @apply text-[32px] font-bold;
+    @apply text-3xl font-bold mb-2;
   }
   > #desc {
-    @apply text-xl font-medium text-center;
+    @apply text-base font-medium text-center leading-[1.2];
   }
 }
 </style>
