60ae31c00f424b42df5f80f66507ccfc3ce953bd
Danish <danish.danish.danish2016@gmail.com>
2023-08-08 01:08:27 +0500

    fixed bug in integrations page

--- src/pages/IntegrationsPage.vue
+++ src/pages/IntegrationsPage.vue
@@ -4,11 +4,11 @@
     desc="Integrate with your existing tools to sync compensation information and workflows. Changes made in one place seamlessly appear in all."
     :bg="integrations_bg"
   />
-  <contactForm />
+  <ContactForm />
 </template>
 <script setup>
 import HeroSection from "@/components/HeroSection.vue";
-import contactForm from "@/components/contactForm.vue";
+import ContactForm from "@/components/ContactForm.vue";
 import integrations_bg from "@/assets/images/integrations_bg.png";
 </script>
 <style lang="postcss" scoped></style>
