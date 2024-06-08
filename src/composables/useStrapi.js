17a72c17f0762ede8ed8b042375b635336262449
danish2016 <danish.danish.danish2016@gmail.com>
2024-06-08 20:41:37 +0500

    Updated strapi endpoint to ssl

--- src/composables/useStrapi.js
+++ src/composables/useStrapi.js
@@ -1,9 +1,11 @@
 const STRAPI_TOKEN =
   "518b5a8f55117cff2f6c988e3d76507cb9bd5aaf65c428367e72587ae03cbdfdf73bffad26f215c602163040cadd340d9797ed7e4ee21f7688424ea8234a321e5be091a5c4038a31216415ce6043b4f449db6dc288fc2b879de69b2ca73a0c34ee17c703b0f1ded10b6398eb4c8557500caef032616d62f204e2c3936d9f9ae2";
 
+const STRAPI_BASE_URL = "https://butfor.co/strapi/api";
+
 function useStrapi() {
   async function getArticles() {
-    const resp = await fetch("http://72.167.37.48:3002/api/articles", {
+    const resp = await fetch(STRAPI_BASE_URL + "/articles", {
       headers: {
         Authorization: `Bearer ${STRAPI_TOKEN}`,
       },
@@ -13,7 +15,7 @@ function useStrapi() {
     return articles;
   }
   async function getArticle(id) {
-    const resp = await fetch(`http://72.167.37.48:3002/api/articles/${id}`, {
+    const resp = await fetch(STRAPI_BASE_URL + `/articles/${id}`, {
       headers: {
         Authorization: `Bearer ${STRAPI_TOKEN}`,
       },
