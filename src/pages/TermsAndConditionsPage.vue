c2028efcceca3906d70e83769d3851a23b07eed1
danish2016 <danish.danish.danish2016@gmail.com>
2024-08-01 07:35:56 +0500

    added terms and conditions page

---
+++ src/pages/TermsAndConditionsPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <!-- terms and conditions -->
+    <section></section>
+
+    <!-- privacy policy -->
+    <section></section>
+  </section>
+</template>
+<script setup></script>
+<style scoped></style>
