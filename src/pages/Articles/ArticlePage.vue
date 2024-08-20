174c174ac256f1f1553aa4a79c6ce408745ad02c
danish2016 <danish.danish.danish2016@gmail.com>
2024-08-20 18:43:54 +0500

    "Refactor ArticlePage.vue: added body content to v-html"

--- src/pages/Articles/ArticlePage.vue
+++ src/pages/Articles/ArticlePage.vue
@@ -2,7 +2,7 @@
   <div class="mx-auto max-w-[1200px] px-2 py-6 sm:px-4 md:px-18">
     <template v-if="!articleId || article == null">
       <div
-        class="text-center py-20 font-medium text-xl flex items-center flex-col gap-4"
+        class="flex flex-col items-center gap-4 py-20 text-center text-xl font-medium"
       >
         <div>
           Article does not exist. <br />
@@ -10,31 +10,29 @@
         </div>
         <RouterLink
           to="/articles"
-          class="text-sm px-2 w-full max-w-xs py-1 bg-primary text-white rounded"
+          class="w-full max-w-xs rounded bg-primary px-2 py-1 text-sm text-white"
           >Go back to articles</RouterLink
         >
       </div>
     </template>
     <template v-else>
       <div
-        class="w-full py-10 flex flex-col items-center"
+        class="flex w-full flex-col items-center py-10"
         v-if="Object.keys(article).length == 0"
       >
         <!-- Loading... -->
         <Spinner />
       </div>
-      <div v-else class="p-10 text-white bg-primary rounded-3xl">
+      <div v-else class="rounded-3xl bg-primary p-10 text-white">
         <div
-          class="py-10 flex justify-between items-center border-b border-white"
+          class="flex items-center justify-between border-b border-white py-10"
         >
           <div class="text-5xl font-bold">
             {{ article.attributes.title }}
           </div>
-          <button class="p-2 rounded-xl bg-primaryLight">Download</button>
+          <button class="rounded-xl bg-primaryLight p-2">Download</button>
         </div>
-        <div class="py-6">
-          {{ article.attributes.body }}
-        </div>
+        <div class="py-6" v-html="article.attributes.body"></div>
       </div>
     </template>
   </div>
