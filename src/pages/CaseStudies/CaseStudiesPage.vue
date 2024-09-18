dd2536bbcade66e592576d4c5d481a36fac498ca
danish2016 <danish.danish.danish2016@gmail.com>
2024-09-18 15:36:41 +0500

    "Updated CaseStudiesPage.vue: modified CSS classes and styles for layout, images, and text elements."

--- src/pages/CaseStudies/CaseStudiesPage.vue
+++ src/pages/CaseStudies/CaseStudiesPage.vue
@@ -8,16 +8,19 @@
       <!-- Loading... -->
       <Spinner />
     </div>
-    <div v-else class="flex flex-wrap justify-center gap-x-4 gap-y-12 py-10">
+    <div
+      v-else
+      class="flex flex-col flex-wrap justify-center gap-x-4 gap-y-12 py-10 lg:flex-row"
+    >
       <div
         v-for="caseStudy in caseStudies"
         :id="caseStudy.id"
-        class="w-[calc(50%-16px)] bg-white shadow-lg"
+        class="w-full bg-white shadow-lg lg:w-[calc(50%-16px)]"
       >
         <!-- :values="caseStudy.attributes" -->
         <div>
           <img
-            class="mx-auto h-60 max-h-60"
+            class="mx-auto h-60 max-h-60 object-contain"
             :src="
               caseStudy.attributes.thumbnail.data
                 ? 'https://butfor.co/strapi' +
@@ -27,16 +30,20 @@
             alt=""
           />
         </div>
-        <div class="p-6">
-          <div class="mb-4 text-[28px] font-semibold leading-[1.2]">
+        <div class="p-3 lg:p-6">
+          <div
+            class="mb-2 text-xl font-semibold leading-[1.2] lg:mb-4 lg:text-[28px]"
+          >
             {{ caseStudy.attributes.title }}
           </div>
-          <div class="font-aneklatin; mb-6 text-lg font-medium text-gray-500">
+          <div
+            class="mb-2 font-aneklatin text-sm font-medium text-gray-500 lg:mb-6 lg:text-lg"
+          >
             {{ trimDesc(caseStudy.attributes.description) }}
           </div>
           <RouterLink
             :to="`/case-studies/${caseStudy.id}`"
-            class="block w-fit rounded border border-primaryLight px-3 py-2 hover:bg-primaryLight hover:text-white"
+            class="block w-fit rounded border border-primaryLight px-3 py-2 text-sm hover:bg-primaryLight hover:text-white lg:text-base"
           >
             Read Now
           </RouterLink>
